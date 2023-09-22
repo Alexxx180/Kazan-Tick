6ebd1d52ce30863c6351c11c5f3cff241c7b80f0
Alexxx180 <alexx120702@gmail.com>
2023-09-22 03:36:05 +0300

    Allowed 120 FPS

--- assets/scripts/actors/controllers/player/model/state.gd
+++ assets/scripts/actors/controllers/player/model/state.gd
@@ -3,13 +3,18 @@ extends Node3D
 @onready var tree = $AnimationPlayer/tree
 @onready var animation = $AnimationPlayer
 
+var last = "run"
+
 func interrupt(state: String):
 	var request = "parameters/character/transition_request"
 	tree.set(request, state)
 	
 func act(action: String):
+	if (action == last):
+		return
 	var request = "parameters/active/transition_request"
 	tree.set(request, action)
+	last = action
 	
 func pause(is_active: bool):
 	if (is_active):
